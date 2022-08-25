87d6427c533cb39c3f48577ef8b9139171b3d438
Adam Arafa Røe <adamarafaroe@nav.no>
2022-08-25 12:14:05 +0200

    endret koden i filen med noen prints for testing

--- airflow/insert_into_ef_stonad_arena.py
+++ airflow/insert_into_ef_stonad_arena.py
@@ -39,17 +39,17 @@ def connection(sql):
         print(error)
 
 
-# def get_periode():
-#     """
-#     henter periode for the tidligere måneden eksample--> i dag er 19.04.2022, metoden vil kalkulerer periode aarMaaned eks) '202203'
-#     :param periode:
-#     :return: periode
-#     """
-#     today = datetime.date.today() # dato for idag 2022-04-19
-#     first = today.replace(day=1) # dato for første dag i måneden 2022-04-01
-#     lastMonth = first - datetime.timedelta(days=1) # dato for siste dag i tidligere måneden
+def get_periode():
+    """
+    henter periode for the tidligere måneden eksample--> i dag er 19.04.2022, metoden vil kalkulerer periode aarMaaned eks) '202203'
+    :param periode:
+    :return: periode
+    """
+    today = datetime.date.today() # dato for idag 2022-04-19
+    first = today.replace(day=1) # dato for første dag i måneden 2022-04-01
+    lastMonth = first - datetime.timedelta(days=1) # dato for siste dag i tidligere måneden
 
-#     return lastMonth.strftime("%Y%m") # henter bare aar og maaned
+    return lastMonth.strftime("%Y%m") # henter bare aar og maaned
 
 
 def delete_data(periode):
@@ -83,7 +83,9 @@ def insert_data():
     connection(sql)
 
 if __name__ == '__main__':
-    periode = 202207 #get_periode()
+    periode = get_periode()
+    print(periode)
+    print(oracle_secrets().user)
     delete_data(periode)
     insert_data()
 
