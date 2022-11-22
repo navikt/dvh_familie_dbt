927eaecb3d056f10e00cf90bf669ec87486dc9c5
Adam Arafa Røe <adamarafaroe@nav.no>
2022-11-22 10:37:01 +0100

    endret sql code

--- models/test/ef_ny_consument_test.sql
+++ models/test/ef_ny_consument_test.sql
@@ -1,30 +1,23 @@
-{{config(
-    materialized='view',
-    tags="ef_kafka_test"
-)}}
+{{ config(
+    tags=["ef_kafka_test"]
+) }}
 
 with kafka_ny_løsning as (
   select * from {{ source ('fam_ef', 'kafka_ny_løsning_test') }}
 ),
+
 
 final as (
 select
 k.kafka_offset,
 k.kafka_topic,
 k.kafka_partisjon,
-k.kafka_hash
-j.fagsakId,
-j.behandlingId,
-j.relatertBehandlingId,
-j.tidspunktVedtak,
-sysdate lastet_dato
-from kafka_ny_løsning k,
-json_table(to_clob(kafka_message), '$'
-  columns(
-        fagsakId PATH '$.fagsakId',
-        behandlingId PATH '$.behandlingId',
-        relatertBehandlingId PATH '$.relatertBehandlingId',
-        tidspunktVedtak PATH '$.tidspunktVedtak')
-        ) j )
+k.kafka_hash,
+k.kafka_message.fagsakId,
+k.kafka_message.behandlingId,
+k.kafka_message.relatertBehandlingId,
+k.kafka_message.tidspunktVedtak
+from KAFKA_NY_LØSNING_TEST k
+)
 
-select * from final
+select * from final;
