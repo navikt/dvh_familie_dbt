afff4020d3503016f9caa600dfa51b18832f7b8c
Adam Arafa Røe <adamarafaroe@nav.no>
2022-11-22 09:01:42 +0100

    endret til debug level istedenfor info

--- airflow/dbt_run_test.py
+++ airflow/dbt_run_test.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
 
     set_secrets_as_envs()
 
-    if not log_level: log_level = 'INFO'
+    if not log_level: log_level = 'DEBUG'
     logger.setLevel(log_level)
     logger.addHandler(stream_handler)
 
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     except subprocess.CalledProcessError as err:
         raise Exception(logger.error(dbt_logg(project_path)),
                        err.stdout.decode("utf-8"))
-       #raise Exception("dbt feiler")
+        #raise Exception("dbt feiler")
 
     filtered_logs = filter_logs(f"{project_path}/logs/dbt.log")
     write_to_xcom_push_file(filtered_logs)
