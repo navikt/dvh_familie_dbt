31f05e62e35d5956595a14cde78a55ef0684d64f
Adam Arafa Røe <adam.arafa.roe@nav.no>
2022-12-01 15:33:46 +0100

    oppdatert test file

---
+++ models/test/ef_data_test.sql
@@ -0,0 +1,22 @@
+{{ config(
+  tags = ["ef_kafka_test"]
+) }}
+
+with kafka_ny_losning as (
+  select * from {{ source ("fam_ef", "kafka_ny_losning_test") }}
+),
+
+final as (
+select
+k.kafka_offset,
+k.kafka_topic,
+k.kafka_partisjon,
+k.kafka_hash,
+k.kafka_message.fagsakId,
+k.kafka_message.behandlingId,
+k.kafka_message.relatertBehandlingId,
+k.kafka_message.tidspunktVedtak
+from kafka_ny_losning k
+)
+
+select * from final
