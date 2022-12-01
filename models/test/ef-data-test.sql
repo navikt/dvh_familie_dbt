452d88dc6e8ea8d620b4614aeaf13963769601e6
Hval <hans.hval@nav.no>
2022-12-01 14:45:23 +0100

    endret tabellnavn

--- models/test/ef-data-test.sql
+++ models/test/ef-data-test.sql
@@ -2,30 +2,26 @@
   tags = ["ef_kafka_test"]
 ) }}
 
-WITH kafka_ny_losning AS (
-
-  SELECT
-    *
-  FROM
-    {{ source (
-      'fam_ef',
-      'kafka_ny_løsning_test'
-    ) }}
+with kafka_ny_losning as (
+  select * from {{ source ('fam_ef', 'kafka_ny_løsning_test') }}
 ),
-FINAL AS (
-  SELECT
-    k.kafka_offset,
-    k.kafka_topic,
-    k.kafka_partisjon,
-    k.kafka_hash,
-    k.kafka_message.fagsakId,
-    k.kafka_message.behandlingId,
-    k.kafka_message.relatertBehandlingId,
-    k.kafka_message.tidspunktVedtak
-  FROM
-    kafka_ny_losning k
+
+
+
+
+final as (
+select
+k.kafka_offset,
+k.kafka_topic,
+k.kafka_partisjon,
+k.kafka_hash,
+k.kafka_message.fagsakId,
+k.kafka_message.behandlingId,
+k.kafka_message.relatertBehandlingId,
+k.kafka_message.tidspunktVedtak
+from kafka_ny_losning k
 )
-SELECT
-  *
-FROM
-  FINAL
+
+
+
+select * from final
