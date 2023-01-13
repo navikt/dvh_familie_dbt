d99a5519ddbc8cbb62157b203706a276e1e36594
Adam Arafa Røe <adamarafaroe@nav.no>
2023-01-13 12:59:48 +0100

    tester utpakking av ks_fagsak_tabellen

---
+++ models/KS_transformasjon/fam_ks_fagsak.sql
@@ -0,0 +1,23 @@
+with kafka_ny_losning as (
+  select * from {{ source ('fam_ks', 'fam_ks_meta_data') }}
+),
+
+final as (
+select
+dvh_fam_ks.HIBERNATE_SEQUENCE.nextval pk_fam_ks_fagsak,
+k.kafka_offset,
+k.melding.fagsakId as fagsak_id,
+k.melding.behandlingId as behandling_id,
+k.melding.tidspunktVedtak as tidspunkt_vedtak,
+k.melding.kategori as kategori,
+k.melding.behandlingType as behandling_type,
+k.melding.funksjonellId as funksjonell_id,
+k.melding.behandlingsÅrsak as behandlings_aarsak,
+k.melding.person.personIdent as person_ident
+k.melding.person.rolle
+k.pk_ks_meta_data as fk_ks_meta_data
+from
+  kafka_ny_losning as k
+)
+
+select * from final
