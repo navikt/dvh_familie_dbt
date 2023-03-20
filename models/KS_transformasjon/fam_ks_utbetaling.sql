db0030f0ba1f67a7f124374e5f81aa7445c70d82
Adam Arafa Røe <adam.arafa.roe@nav.no>
2023-03-20 09:00:07 +0100

    all models to incrementa

--- models/KS_transformasjon/fam_ks_utbetaling.sql
+++ models/KS_transformasjon/fam_ks_utbetaling.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='pk_ks_utbetaling'
+    )
+}}
+
 with kafka_ny_losning as (
   select kafka_offset, kafka_mottatt_dato, melding from {{ source ('fam_ks', 'fam_ks_meta_data') }}
 ),
@@ -33,3 +40,9 @@ from pre_final
 )
 
 select * from final
+
+{% if is_incremental() %}
+
+  where kafka_mottatt_dato > (select max(kafka_mottatt_dato) from {{ this }})
+
+{% endif %}
--- models/KS_transformasjon/fam_ks_utbetalings_det.sql
+++ models/KS_transformasjon/fam_ks_utbetalings_det.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='pk_fam_ks_utbet_det'
+    )
+}}
+
 with kafka_ny_losning as (
   select kafka_offset, kafka_mottatt_dato, melding from {{ source ('fam_ks', 'fam_ks_meta_data') }}
 ),
@@ -71,3 +78,9 @@ select
   fk_ks_utbetaling,
   fk_fam_ks_fagsak
 from final
+
+{% if is_incremental() %}
+
+  where kafka_mottatt_dato > (select max(kafka_mottatt_dato) from {{ this }})
+
+{% endif %}
