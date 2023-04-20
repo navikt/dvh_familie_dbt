ef26b38486099a573f1d135cb66ac4349b68fe64
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-04-20 09:47:03 +0200

    endret håndtering av tidspunkt_vedtak i fagsak tabellen

--- models/KS_transformasjon/fam_ks_fagsak.sql
+++ models/KS_transformasjon/fam_ks_fagsak.sql
@@ -65,8 +65,8 @@ select
   fagsak_id,
   behandlings_id,
   CASE
-    WHEN LENGTH(tidspunkt_vedtak) = 29 THEN CAST(to_timestamp_tz(tidspunkt_vedtak, 'FXYYYY-MM-DD"T"HH24:MI:SS.FXFF3TZH:TZM') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP)
     WHEN LENGTH(tidspunkt_vedtak) = 25 THEN CAST(to_timestamp_tz(tidspunkt_vedtak, 'yyyy-mm-dd"T"hh24:mi:ss TZH:TZM') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP)
+    ELSE CAST(to_timestamp_tz(tidspunkt_vedtak, 'FXYYYY-MM-DD"T"HH24:MI:SS.FXFF3TZH:TZM') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP)
     END tidspunkt_vedtak,
   kategori,
   behandling_type,
