8cc2ec650d47c6d6cce9a14eb49694a4d9331e08
Adam Arafa Røe <adam.arafa.roe@nav.no>
2023-07-11 11:57:42 +0200

    BT utpakking kjører på tids invtervall

--- models/Barnetrygd_utpakking/fam_bt_person.sql
+++ models/Barnetrygd_utpakking/fam_bt_person.sql
@@ -8,13 +8,6 @@ with barnetrygd_meta_data as (
   select pk_bt_meta_data, kafka_offset, kafka_mottatt_dato, melding from {{ source ('fam_bt', 'fam_bt_meta_data') }}
   where kafka_mottatt_dato between to_timestamp('{{ var("dag_interval_start") }}', 'yyyy-mm-dd hh24:mi:ss')
   and to_timestamp('{{ var("dag_interval_end") }}', 'yyyy-mm-dd hh24:mi:ss')
-
-  --{% if is_incremental() %}
-
-  --where kafka_mottatt_dato > (select max(kafka_mottatt_dato) from {{ this }})
-
-  --{% endif %}
-
 ),
 
 pre_final_fagsak_person as (
