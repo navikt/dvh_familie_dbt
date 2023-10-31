7d85fe73dde2110741b625b03ef1826e37a0c366
AArafaR <97885051+AArafaR@users.noreply.github.com>
2023-10-31 11:00:38 +0100

    ny format for å håndtere vedtaks_tidspunkt

--- models/PP_utpakking/fam_pp_fagsak.sql
+++ models/PP_utpakking/fam_pp_fagsak.sql
@@ -38,11 +38,7 @@ mottaker_final as (
     ,nvl(ident.fk_person1, -1) as FK_PERSON1_MOTTAKER
     ,p.kafka_partition
     ,p.pk_pp_meta_data as fk_pp_metadata
-    ,CASE
-    WHEN LENGTH(p.vedtaks_tidspunkt) = 25 THEN CAST(to_timestamp_tz(p.vedtaks_tidspunkt, 'yyyy-mm-dd"T"hh24:mi:ss TZH:TZM') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP)
-    WHEN '.' NOT IN p.vedtaks_tidspunkt THEN CAST(to_timestamp_tz(p.vedtaks_tidspunkt, 'yyyy-mm-dd"T"hh24:mi:ss TZH:TZM') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP)
-    ELSE CAST(to_timestamp_tz(p.vedtaks_tidspunkt, 'FXYYYY-MM-DD"T"HH24:MI:SS.FXFFTZH:TZM') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP)
-    END vedtaks_tidspunkt
+    ,CAST(to_timestamp_tz(p.vedtaks_tidspunkt, 'yyyy-mm-dd"T"hh24:mi:ss.ff3') AT TIME ZONE 'Europe/Belgrade' AS TIMESTAMP) vedtaks_tidspunkt
     ,p.forrige_behandlings_id
   from pre_final p
   left outer join dt_person.ident_off_id_til_fk_person1 ident
