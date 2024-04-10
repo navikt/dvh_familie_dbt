5c76500d8cd74a72f9d1c01b0b0629060a4cff18
Rong <Helen.Rong@nav.no>
2024-04-10 16:31:44 +0200

    Lagt inn kafka_partition i fp_meldinger_til_aa_pakke_ut

--- models/FP_utpakking/fp_meldinger_til_aa_pakke_ut.sql
+++ models/FP_utpakking/fp_meldinger_til_aa_pakke_ut.sql
@@ -5,7 +5,7 @@
 }}
 
 with fp_meta_data as (
-  select pk_fp_meta_data, kafka_offset, kafka_mottatt_dato, melding from {{ source ('fam_fp', 'fam_fp_meta_data') }}
+  select pk_fp_meta_data, kafka_offset, kafka_mottatt_dato, kafka_partition, melding from {{ source ('fam_fp', 'fam_fp_meta_data') }}
   --where kafka_offset in (4586, 4596)
   where kafka_mottatt_dato >= sysdate - 30
   and (kafka_offset, kafka_partition) not in (select distinct kafka_offset, kafka_partition from {{ source('fam_fp','json_fam_fp_fagsak') }})
