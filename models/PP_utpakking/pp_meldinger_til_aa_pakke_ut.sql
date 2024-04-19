d91ca11e0e406aee4bc62b5a79a4a27ec17601a0
Rong <Helen.Rong@nav.no>
2024-04-19 16:27:25 +0200

    Pakk ut pp finnes i test_offset

--- models/PP_utpakking/pp_meldinger_til_aa_pakke_ut.sql
+++ models/PP_utpakking/pp_meldinger_til_aa_pakke_ut.sql
@@ -6,8 +6,10 @@
 
 with pleiepenger_meta_data as (
   select pk_pp_meta_data, kafka_offset, kafka_mottatt_dato, kafka_topic, KAFKA_PARTITION, melding from {{ source ('fam_pp', 'fam_pp_meta_data') }}
-    where kafka_mottatt_dato >= sysdate - 30 and kafka_offset not in (
-      select kafka_offset from {{ source ('fam_pp', 'fam_pp_fagsak') }})
+    --where kafka_mottatt_dato >= sysdate - 30 and kafka_offset not in (
+      --select kafka_offset from {{ source ('fam_pp', 'fam_pp_fagsak') }})
+      where kafka_offset in (select kafka_offset from test_offset)
+      and kafka_offset not in (select kafka_offset from {{ source ('fam_pp', 'fam_pp_fagsak') }})
 )
 
 select * from pleiepenger_meta_data
