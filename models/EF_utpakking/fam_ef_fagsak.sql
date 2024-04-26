2a2dae59ba1e250516fd6aa42ba7a9be9f9372d9
Gard <Gard.Sigurd.Troim.Henriksen@nav.no>
2024-04-26 10:57:45 +0200

    La til EØS endringer fra dev + endringer for profiles & setup

--- models/EF_utpakking/fam_ef_fagsak.sql
+++ models/EF_utpakking/fam_ef_fagsak.sql
@@ -12,12 +12,12 @@ pre_final as (
 select * from ef_meta_data,
   json_table(melding, '$'
     COLUMNS (
-        fagsak_id                       VARCHAR2 PATH '$.fagsakId'
+        fagsak_id                        VARCHAR2 PATH '$.fagsakId'
         ,BEHANDLINGS_ID                  VARCHAR2 PATH '$.behandlingId'
         ,relatert_behandlings_id         VARCHAR2 PATH '$.relatertBehandlingId'
         ,adressebeskyttelse              VARCHAR2 PATH '$.adressebeskyttelse'
         ,behandling_type                 VARCHAR2 PATH '$.behandlingType'
-        ,behandlings_aarsak               VARCHAR2 PATH '$.behandlingÅrsak'
+        ,behandlings_aarsak              VARCHAR2 PATH '$.behandlingÅrsak'
         ,vedtaks_status                  VARCHAR2 PATH '$.vedtak'
         ,stonadstype                     VARCHAR2 PATH '$.stønadstype'
         ,person_ident                    VARCHAR2 PATH '$.person.personIdent'
@@ -26,9 +26,12 @@ select * from ef_meta_data,
         ,funksjonell_id                  VARCHAR2 PATH '$.funksjonellId'
         ,vedtaks_tidspunkt               VARCHAR2 PATH '$.tidspunktVedtak'
         ,aktivitetsvilkaar_barnetilsyn   VARCHAR2 PATH '$.aktivitetskrav'
-        ,krav_mottatt                     VARCHAR2 PATH '$.kravMottatt'
+        ,krav_mottatt                    VARCHAR2 PATH '$.kravMottatt'
         ,årsak_revurderings_kilde        VARCHAR2 PATH '$.årsakRevurdering.opplysningskilde'
         ,revurderings_årsak              VARCHAR2 PATH '$.årsakRevurdering.årsak'
+        ,medlem_Mer_Enn_5_Aar_Eos        VARCHAR2 PATH '$.eøsUnntak.medlemMerEnn5ÅrEøs'
+        ,medlem_Mer_Enn_5_Aar_Eos_Annen_Forelder_Trygdedekket_I_Norge VARCHAR2 PATH '$.eøsUnntak.medlemMerEnn5ÅrEøsAnnenForelderTrygdedekketINorge'
+        ,oppholder_Seg_I_Annet_Eos_Land  VARCHAR2 PATH '$.eøsUnntak.oppholderSegIAnnetEøsLand'
          )
         ) j
 ),
@@ -40,6 +43,9 @@ final as (
     ,p.relatert_behandlings_id
     ,p.adressebeskyttelse
     ,p.behandling_type
+    ,p.medlem_Mer_Enn_5_Aar_Eos
+    ,p.medlem_Mer_Enn_5_Aar_Eos_Annen_Forelder_Trygdedekket_I_Norge
+    ,p.oppholder_Seg_I_Annet_Eos_Land
     ,p.behandlings_aarsak
     ,p.vedtaks_status
     ,p.stonadstype
@@ -72,21 +78,8 @@ final as (
 
 select dvh_famef_kafka.hibernate_sequence.nextval as PK_EF_FAGSAK
   ,FK_EF_META_DATA
-  ,FAGSAK_ID
-  ,BEHANDLINGS_ID
-  ,RELATERT_BEHANDLINGS_ID
-  ,ADRESSEBESKYTTELSE
   ,FK_PERSON1
-  ,BEHANDLING_TYPE
-  ,BEHANDLINGS_AARSAK
-  ,VEDTAKS_STATUS
-  ,STONADSTYPE
-  ,case when FK_PERSON1 = -1 then PERSON_IDENT
-      else cast(null as varchar2(11))
-  end PERSON_IDENT
   ,AKTIVITETSPLIKT_INNTREFFER_DATO
-  ,HAR_SAGT_OPP_ARBEIDSFORHOLD
-  ,FUNKSJONELL_ID
   ,VEDTAKS_TIDSPUNKT
   ,KAFKA_TOPIC
   ,KAFKA_OFFSET
@@ -94,14 +87,31 @@ select dvh_famef_kafka.hibernate_sequence.nextval as PK_EF_FAGSAK
   ,localtimestamp AS lastet_dato
   ,cast(null as varchar2(20)) FAGSAK_ID_GML
   ,cast(null as varchar2(20)) BEHANDLINGS_ID_GML
+  ,KRAV_MOTTATT
+  ,FAGSAK_ID
+  ,BEHANDLINGS_ID
+  ,RELATERT_BEHANDLINGS_ID
+  ,ADRESSEBESKYTTELSE
+  ,BEHANDLING_TYPE
+  ,BEHANDLINGS_AARSAK
+  ,VEDTAKS_STATUS
+  ,STONADSTYPE
+  ,case when FK_PERSON1 = -1 then PERSON_IDENT
+      else cast(null as varchar2(11))
+  end PERSON_IDENT
+  ,HAR_SAGT_OPP_ARBEIDSFORHOLD
+  ,FUNKSJONELL_ID
   ,AKTIVITETSVILKAAR_BARNETILSYN
   ,cast(null as varchar2(20)) VEDTAKSBEGRUNNELSE_SKOLE
-  ,KRAV_MOTTATT
   ,ÅRSAK_REVURDERINGS_KILDE
   ,REVURDERINGS_ÅRSAK
+  ,case when medlem_Mer_Enn_5_Aar_Eos = 'true' then 1
+  else 0
+  end medlem_Mer_Enn_5_Aar_Eos
+  ,case when oppholder_Seg_I_Annet_Eos_Land = 'true' then 1
+  else 0
+  end oppholder_Seg_I_Annet_Eos_Land
+  ,case when medlem_Mer_Enn_5_Aar_Eos_Annen_Forelder_Trygdedekket_I_Norge = 'true' then 1
+  else 0
+  end medlem_Mer_Enn_5_Aar_Eos_Annen_Forelder_Trygdedekket_I_Norge
 from final
-
-
-
-
-
--- setup_db_user.ps1
+++ setup_db_user.ps1
@@ -6,11 +6,12 @@ $username = $creds.Username
 if ($schema) {
   $username += "[$schema]"
   $env:DBT_DB_SCHEMA = $schema
-} else {
+}
+else {
   $env:DBT_DB_SCHEMA = $username
 }
-$env:DBT_DB_USER = $username
-$env:DBT_DB_PASS = $creds.GetNetworkCredential().password
+$env:DBT_ENV_SECRET_USER = $username
+$env:DBT_ENV_SECRET_PASS = $creds.GetNetworkCredential().password
 $env:DBT_DB_TARGET = $target
 
 $env:DBT_PROFILES_DIR = Get-Location
