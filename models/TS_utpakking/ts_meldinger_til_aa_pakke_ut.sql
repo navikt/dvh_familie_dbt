a0591c11e0bef880bf05fb90438b205568333f91
Adam Arafa Røe <adam.arafa.roe@nav.no>
2024-06-17 15:05:02 +0200

    pakker ut kun meldinger som har overgangsstønad som type i malgrupper

--- models/TS_utpakking/ts_meldinger_til_aa_pakke_ut.sql
+++ models/TS_utpakking/ts_meldinger_til_aa_pakke_ut.sql
@@ -5,8 +5,17 @@
 }}
 
 with ts_meta_data as (
-  select pk_ts_meta_data, melding, ekstern_behandling_id, endret_tid from {{ source ('fam_ef', 'fam_ts_meta_data') }}
-    where endret_tid > nvl( (select max(endret_tid) from {{ source ('fam_ef', 'fam_ts_fagsak') }}), endret_tid-1 )
+  SELECT m.*
+    FROM {{ source ('fam_ef', 'fam_ts_meta_data') }} m,
+        JSON_TABLE(
+            m.melding,
+            '$.malgrupper.type[*]'
+            COLUMNS(
+                type VARCHAR2(100) PATH '$'
+            )
+        ) j
+    WHERE j.type = 'OVERGANGSSTØNAD'
+    AND endret_tid > nvl( (select max(endret_tid) from {{ source ('fam_ef', 'fam_ts_fagsak') }}), endret_tid-1 )
     --where opprettet_tid >= sysdate - 30 and ekstern_behandling_id not in (
       --select ekstern_behandling_id from {{ source ('fam_ef', 'fam_ts_fagsak') }})
         --and endret_tid <= (select max(endret_tid) from {{ this }})
