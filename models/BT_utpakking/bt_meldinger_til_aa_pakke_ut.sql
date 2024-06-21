e06174a2865a2deacc6dffd11f47374d6e8b4bd4
Adam Arafa Røe <adam.arafa.roe@nav.no>
2024-06-21 09:39:26 +0200

    lagt begrensing i meldinger til å pakke ut

--- models/BT_utpakking/bt_meldinger_til_aa_pakke_ut.sql
+++ models/BT_utpakking/bt_meldinger_til_aa_pakke_ut.sql
@@ -5,10 +5,21 @@
 }}
 
 with barnetrygd_meta_data as (
-  select pk_bt_meta_data, kafka_offset, kafka_mottatt_dato, melding from {{ source ('fam_bt', 'fam_bt_meta_data') }}
-    where kafka_mottatt_dato >= sysdate - 30 and kafka_offset not in (
-      select kafka_offset from {{ source ('fam_bt', 'fam_bt_fagsak') }})
+select pk_bt_meta_data, kafka_offset, kafka_mottatt_dato, melding
+from
+(
+  select meta.*
+        ,json_value(melding, '$.behandlingsId') behandlingsId
+        ,row_number() over (partition by json_value(melding, '$.behandlingsId') order by kafka_offset desc) nr
+  from {{ source ('fam_bt', 'fam_bt_meta_data') }} meta
+  where kafka_mottatt_dato >= sysdate - 30
+)
+where nr = 1
+and kafka_offset not in (select kafka_offset from {{ source ('fam_bt', 'fam_bt_fagsak') }})
 )
 
 select * from barnetrygd_meta_data
+
+
+
 
