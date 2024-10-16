1766b8606749ebe6e9cba92f698a67e53491c572
AArafaR <97885051+AArafaR@users.noreply.github.com>
2024-10-16 13:48:27 +0200

    kjører med en ny versjon av dbt_run.py

--- airflow/dbt_run.py
+++ airflow/dbt_run.py
@@ -1,93 +1,70 @@
-import subprocess
+import requests
 import os
 import time
 import json
-import sys
 import logging
-from typing import List
-from google.cloud import secretmanager
+from pathlib import Path
 import shlex
+from google.cloud import secretmanager
+
+
 from dbt.cli.main import dbtRunner, dbtRunnerResult
+
 
 DBT_BASE_COMMAND = ["--no-use-colors", "--log-format-file", "json"]
 
-def set_secrets_as_envs():
-  secrets = secretmanager.SecretManagerServiceClient()
-  resource_name = f"{os.environ['KNADA_TEAM_SECRET']}/versions/latest"
-  secret = secrets.access_secret_version(name=resource_name)
-  secret_str = secret.payload.data.decode('UTF-8')
-  secrets = json.loads(secret_str)
-  os.environ.update(secrets)
+
+def get_dbt_log(log_path) -> str:
+    with open(log_path) as log:
+        return log.read()
 
 
-def write_to_xcom_push_file(content: List[dict]):
-    with open('/airflow/xcom/return.json', 'w') as xcom_file:
-        json.dump(content, xcom_file)
+def set_secrets_as_envs(secret_name: str):
+    secrets = secretmanager.SecretManagerServiceClient()
+    resource_name = f"{os.environ['KNADA_TEAM_SECRET']}/versions/latest"
+    secret = secrets.access_secret_version(name=resource_name)
+    secret_str = secret.payload.data.decode('UTF-8')
+    secrets = json.loads(secret_str)
+    os.environ.update(secrets)
 
-
-def filter_logs(file_path: str) -> List[dict]:
-    logs = []
-    with open(file_path) as logfile:
-      for log in logfile:
-        logs.append(json.loads(log))
-
-    dbt_codes = [
-      'Q009', #PASS
-      'Q010', #WARN
-      'Q011', #FAIL
-      'Q019', #Freshness WARN
-      'Q020', #Freshness PASS
-      'Z021', #Info about warning in tests
-      'Z022', #Info about failing tests
-      'E040', #Total runtime
-    ]
-
-    filtered_logs = [log for log in logs if log['info']['code'] in dbt_codes]
-
-    return filtered_logs
-
+logging.basicConfig(
+    format='{"msg":"%(message)s", "time":"%(asctime)s", "level":"%(levelname)s"}',
+    force=True,
+    level=logging.getLevelName("INFO"),
+)
 
 if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    stream_handler = logging.StreamHandler(sys.stdout)
+    set_secrets_as_envs()
+    log_path = Path(__file__).parent / "logs/dbt.log"
+
     os.environ["TZ"] = "Europe/Oslo"
     time.tzset()
-    profiles_dir = str(sys.path[0])
-    command = shlex.split(os.environ["DBT_COMMAND"])
+
+    schema = os.getenv("DB_SCHEMA")
+    os.environ["DBT_ORCL_USER_PROXY"] = f"{os.environ['DB_USER']}[{schema}]"
+    os.environ["DBT_ORCL_SCHEMA"] = schema
+    #os.environ["DBT_DB_DSN"] = os.environ["DB_DSN"]
+    os.environ["DBT_ORCL_PASS"] = os.environ["DB_PASSWORD"]
+    logging.info("DBT miljøvariabler er lastet inn")
+
+    # default dbt kommando er build
+    command = shlex.split(os.getenv("DBT_COMMAND", "run"))
+    if dbt_models := os.getenv("DBT_MODELS", None):
+        command = command + ["--select", dbt_models]
+
     dbt = dbtRunner()
     dbt_deps = dbt.invoke(DBT_BASE_COMMAND + ["deps"])
     output: dbtRunnerResult = dbt.invoke(DBT_BASE_COMMAND + command)
 
-    #[c.replace('\\', '') for c in os.environ["DBT_COMMAND"].split()]
-    log_level = os.getenv("LOG_LEVEL")
-    schema = os.getenv("DB_SCHEMA")
-
-    set_secrets_as_envs() #get secrets from gcp
-
-    if not log_level: log_level = 'INFO'
-    logger.setLevel(log_level)
-    logger.addHandler(stream_handler)
-
-    #logger.debug(f"dbt command: {command}")
-    #logger.debug(f"db schema: {schema}")
-
-    def dbt_logg(my_path) -> str:
-      with open(my_path + "/logs/dbt.log") as log: return log.read()
-
-    # setter miljø og korrekt skjema med riktig proxy
-    os.environ['DBT_ORCL_USER_PROXY'] = f"{os.environ['DBT_ORCL_USER']}" + (f"[{schema}]" if schema else '')
-    os.environ['DBT_ORCL_SCHEMA'] = (schema if schema else os.environ['DBT_ORCL_USER']) # samme som i Vault
-
-    logger.info(f"bruker: {os.environ['DBT_ORCL_USER_PROXY']}")
-
-    project_path = os.path.dirname(os.getcwd())
-    logger.info(f"Prosjekt path er: {project_path}")
-
-   # Exit code 2, feil utenfor DBT
+    # Exit code 2, feil utenfor DBT
     if output.exception:
         raise output.exception
     # Exit code 1, feil i dbt (test eller under kjøring)
     if not output.success:
         raise Exception(output.result)
 
+    if "docs" in command:
+        dbt_project = os.environ["DBT_DOCS_PROJECT_NAME"]
+        logging.info("publiserer dbt docs")
 
+    # Legg til logikk for å skrive logg til xcom
