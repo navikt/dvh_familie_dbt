45a085e855af6ec9cbb751192fdd71d6fab1d51f
AArafaR <97885051+AArafaR@users.noreply.github.com>
2024-10-17 13:13:07 +0200

    oppdatert dbt_run med en ekstra sub process

--- airflow/dbt_run.py
+++ airflow/dbt_run.py
@@ -9,40 +9,28 @@ from google.cloud import secretmanager
 import shlex
 
 def set_secrets_as_envs():
-  secrets = secretmanager.SecretManagerServiceClient()
-  resource_name = f"{os.environ['KNADA_TEAM_SECRET']}/versions/latest"
-  secret = secrets.access_secret_version(name=resource_name)
-  secret_str = secret.payload.data.decode('UTF-8')
-  secrets = json.loads(secret_str)
-  os.environ.update(secrets)
-
+    secrets = secretmanager.SecretManagerServiceClient()
+    resource_name = f"{os.environ['KNADA_TEAM_SECRET']}/versions/latest"
+    secret = secrets.access_secret_version(name=resource_name)
+    secret_str = secret.payload.data.decode('UTF-8')
+    secrets = json.loads(secret_str)
+    os.environ.update(secrets)
 
 def write_to_xcom_push_file(content: List[dict]):
     with open('/airflow/xcom/return.json', 'w') as xcom_file:
         json.dump(content, xcom_file)
 
-
 def filter_logs(file_path: str) -> List[dict]:
     logs = []
     with open(file_path) as logfile:
-      for log in logfile:
-        logs.append(json.loads(log))
+        for log in logfile:
+            logs.append(json.loads(log))
 
     dbt_codes = [
-      'Q009', #PASS
-      'Q010', #WARN
-      'Q011', #FAIL
-      'Q019', #Freshness WARN
-      'Q020', #Freshness PASS
-      'Z021', #Info about warning in tests
-      'Z022', #Info about failing tests
-      'E040', #Total runtime
+        'Q009', 'Q010', 'Q011', 'Q019', 'Q020', 'Z021', 'Z022', 'E040',
     ]
-
     filtered_logs = [log for log in logs if log['info']['code'] in dbt_codes]
-
     return filtered_logs
-
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
@@ -52,7 +40,7 @@ if __name__ == "__main__":
     profiles_dir = str(sys.path[0])
     command = shlex.split(os.environ["DBT_COMMAND"])
 
-    #[c.replace('\\', '') for c in os.environ["DBT_COMMAND"].split()]
+
     log_level = os.getenv("LOG_LEVEL")
     schema = os.getenv("DB_SCHEMA")
 
@@ -62,38 +50,36 @@ if __name__ == "__main__":
     logger.setLevel(log_level)
     logger.addHandler(stream_handler)
 
-    #logger.debug(f"dbt command: {command}")
-    #logger.debug(f"db schema: {schema}")
-
     def dbt_logg(my_path) -> str:
       with open(my_path + "/logs/dbt.log") as log: return log.read()
 
-    # setter miljø og korrekt skjema med riktig proxy
     os.environ['DBT_ORCL_USER_PROXY'] = f"{os.environ['DBT_ORCL_USER']}" + (f"[{schema}]" if schema else '')
-    os.environ['DBT_ORCL_SCHEMA'] = (schema if schema else os.environ['DBT_ORCL_USER']) # samme som i Vault
+    os.environ['DBT_ORCL_SCHEMA'] = schema if schema else os.environ['DBT_ORCL_USER']
 
     logger.info(f"bruker: {os.environ['DBT_ORCL_USER_PROXY']}")
-
     project_path = os.path.dirname(os.getcwd())
     logger.info(f"Prosjekt path er: {project_path}")
 
     try:
-        process_deps = subprocess.Popen(['dbt', 'deps'])
+        # Start dbt deps process
+        process_deps = subprocess.Popen(['dbt', 'deps'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Run the main dbt command
         output = subprocess.run(
-            (
-              ["dbt", "--no-use-colors", "--log-format", "json"] +
-              command +
-              ["--profiles-dir", profiles_dir, "--project-dir", project_path]
-            ),
+            ["dbt", "--no-use-colors", "--log-format", "json"] + command +
+            ["--profiles-dir", profiles_dir, "--project-dir", project_path],
             check=True, capture_output=True
         )
-        process_deps.wait()
-        logger.info(output.stdout.decode("utf-8"))
-        logger.debug(dbt_logg(project_path))
-    except subprocess.CalledProcessError as err:
-        raise Exception(logger.error(dbt_logg(project_path)),
-                       err.stdout.decode("utf-8"))
-       #raise Exception("dbt feiler")
 
-    #filtered_logs = filter_logs(f"{project_path}/logs/dbt.log")
-    #write_to_xcom_push_file(filtered_logs)
+        # Wait for dbt deps to complete
+        stdout, stderr = process_deps.communicate()
+        logger.info(stdout.decode("utf-8"))
+        if stderr:
+            logger.error(stderr.decode("utf-8"))
+
+        logger.info(output.stdout.decode("utf-8"))
+
+    except subprocess.CalledProcessError as err:
+        logger.error(dbt_logg(project_path))
+        logger.error(err.stderr.decode("utf-8"))
+        raise
