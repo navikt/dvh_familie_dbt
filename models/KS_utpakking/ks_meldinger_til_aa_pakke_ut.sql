db07c988d8770c9458d67acbf52cfd0664282476
Helen Rong <helen.rong@nav.no>
2025-01-13 16:44:35 +0100

    Pakk ut alle manglende vilkår data for KS

--- models/KS_utpakking/ks_meldinger_til_aa_pakke_ut.sql
+++ models/KS_utpakking/ks_meldinger_til_aa_pakke_ut.sql
@@ -5,8 +5,18 @@
 }}
 
 with kontantstotte_meta_data as (
-  select pk_ks_meta_data, kafka_offset, kafka_mottatt_dato, kafka_topic, kafka_partisjon, melding from {{ source ('fam_ks', 'fam_ks_meta_data') }}
-    where kafka_offset in (71235,71236)
+  select pk_ks_meta_data, kafka_offset, kafka_mottatt_dato, kafka_topic, kafka_partisjon, melding
+  from {{ source ('fam_ks', 'fam_ks_meta_data') }}
+  where pk_ks_meta_data in
+  (
+    select fk_ks_meta_data
+    from {{ source ('fam_ks', 'fam_ks_fagsak') }}
+    where pk_ks_fagsak not in
+    (
+      select fk_ks_fagsak
+      from {{ source ('fam_ks', 'fam_ks_vilkaar_resultat') }}
+    )
+  )
 
 )
 
