e12245d531bcb71e332166cacd48c353e6e30eb0
Helen Rong <helen.rong@nav.no>
2025-01-15 15:54:30 +0100

    Utpakke manglende vilkår og rulle tilbake endringen etter utpakking. Det gjelder fam_ks_vilkaar_resultat, ks_meldinger_til_aa_pakke_ut og schema

--- models/KS_utpakking/ks_meldinger_til_aa_pakke_ut.sql
+++ models/KS_utpakking/ks_meldinger_til_aa_pakke_ut.sql
@@ -7,23 +7,12 @@
 with kontantstotte_meta_data as (
   select pk_ks_meta_data, kafka_offset, kafka_mottatt_dato, kafka_topic, kafka_partisjon, melding
   from {{ source ('fam_ks', 'fam_ks_meta_data') }}
-  --where kafka_mottatt_dato >= sysdate - 30
-  --and kafka_offset not in
-  --(
-    --select kafka_offset from {{ source ('fam_ks', 'fam_ks_fagsak') }}
-  --)
-  --and kafka_offset >= 31172
-
-  where pk_ks_meta_data in
+  where kafka_mottatt_dato >= sysdate - 30
+  and kafka_offset not in
   (
-    select distinct fk_ks_meta_data
-    from {{ source ('fam_ks', 'fam_ks_fagsak') }}
-    where pk_ks_fagsak not in
-    (
-      select distinct fk_ks_fagsak
-      from {{ source ('fam_ks', 'fam_ks_vilkaar_resultat') }}
-    )
+    select kafka_offset from {{ source ('fam_ks', 'fam_ks_fagsak') }}
   )
+  and kafka_offset >= 31172
 )
 
 select * from kontantstotte_meta_data
