cfb8c63d19214a26f518541b6444d9c1bcbb4a2a
Helen Rong <helen.rong@nav.no>
2025-01-15 15:47:21 +0100

    Utpakke manglende vilkår og rulle tilbake endringen etter utpakking. Det gjelder fam_ks_vilkaar_resultat, ks_meldinger_til_aa_pakke_ut og schema

--- models/KS_utpakking/fam_ks_vilkaar_resultat.sql
+++ models/KS_utpakking/fam_ks_vilkaar_resultat.sql
@@ -18,15 +18,15 @@ pre_final as (
     select *  from ks_meta_data,
       json_table(melding, '$'
         columns(
-          behandlings_id  path  '$.behandlingsId',
+          behandlings_id  number(38,0) path  '$.behandlingsId',
           nested          path '$.vilkårResultater[*]'
           columns(
-            resultat              path '$.resultat',
-            antall_timer          path '$.antallTimer',
-            periode_fom           path '$.periodeFom',
-            periode_tom           path '$.periodeTom',
-            ident                 path '$.ident',
-            vilkaar_type          path '$.vilkårType'
+            resultat      varchar2(255) path '$.resultat',
+            antall_timer  number(10,2) path '$.antallTimer',
+            periode_fom   varchar2(255) path '$.periodeFom',
+            periode_tom   varchar2(255) path '$.periodeTom',
+            ident         varchar2(255) path '$.ident',
+            vilkaar_type  varchar2(255) path '$.vilkårType'
             )
           )
         ) j
@@ -37,39 +37,34 @@ pre_final as (
 ),
 
 final as (
-  Select
-  to_number(pre_final.behandlings_id) as fk_ks_fagsak,
-  resultat,
-  ident,
-  replace(antall_timer, '.', ',') antall_timer,
-  --case when antall_timer is not null
-  --  then to_number(antall_timer)
-  --else antall_timer
-  --end antall_timer,
-  to_date(periode_fom, 'yyyy-mm-dd') periode_fom,
-  to_date(periode_tom, 'yyyy-mm-dd') periode_tom,
-  nvl(b.fk_person1, -1) fk_person1,
-  vilkaar_type
-from
-  pre_final
-left outer join dt_person.ident_off_id_til_fk_person1 b on
-  pre_final.ident=b.off_id
-  and b.gyldig_fra_dato<=pre_final.kafka_mottatt_dato
-  and b.gyldig_til_dato>=kafka_mottatt_dato
-  and b.skjermet_kode=0
+  select
+    to_number(pre_final.behandlings_id) as fk_ks_fagsak,
+    resultat,
+    ident,
+    --replace(antall_timer, '.', ',') antall_timer,
+    antall_timer,
+    to_date(periode_fom, 'yyyy-mm-dd') periode_fom,
+    to_date(periode_tom, 'yyyy-mm-dd') periode_tom,
+    nvl(b.fk_person1, -1) fk_person1,
+    vilkaar_type
+  from pre_final
+  left outer join dt_person.ident_off_id_til_fk_person1 b
+  on pre_final.ident = b.off_id
+  and pre_final.kafka_mottatt_dato between b.gyldig_fra_dato and b.gyldig_til_dato
+  and b.skjermet_kode = 0
 )
 
-SELECT
-  dvh_fam_ks.hibernate_sequence.nextval as PK_KS_VILKAAR_RESULTAT,
-  RESULTAT,
-  ANTALL_TIMER,
-  PERIODE_FOM,
-  PERIODE_TOM,
-  case when FK_PERSON1 = -1 then IDENT
-      else cast(null as varchar2(11))
-  end IDENT,
-  FK_PERSON1,
-  VILKAAR_TYPE,
-  localtimestamp AS LASTET_DATO,
+select
+  dvh_fam_ks.hibernate_sequence.nextval as pk_ks_vilkaar_resultat,
+  resultat,
+  antall_timer,
+  periode_fom,
+  periode_tom,
+  case when fk_person1 = -1 then ident
+       else cast(null as varchar2(11))
+  end ident,
+  fk_person1,
+  vilkaar_type,
+  localtimestamp AS lastet_dato,
   fk_ks_fagsak
 from final
