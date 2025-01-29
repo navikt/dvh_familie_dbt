b84e3c8ae1dccc82b1dda5ed35532c3ce2852049
Helen Rong <helen.rong@nav.no>
2025-01-29 12:11:35 +0100

    OMP

--- models/PP_manedsprosessering/fak_pp_omp.sql
+++ models/PP_manedsprosessering/fak_pp_omp.sql
@@ -1,14 +1,13 @@
 {{
     config(
-        materialized='table',
-        schema='pp_omp'
+        materialized='incremental'
     )
 }}
 
 with perioder as
 (
   select *
-  from {{ source('fam_pp', 'fam_pp_perioder') }}
+  from {{ source('fam_pp_omp', 'fam_pp_perioder') }}
 ),
 
 tid as
@@ -20,30 +19,33 @@ tid as
 fagsak as
 (
   select *
-  from {{ source('fam_pp', 'fam_pp_fagsak') }}
+  from {{ source('fam_pp_omp', 'fam_pp_fagsak') }}
 ),
 
 vedtak1 as
 (
   select fagsak.pk_pp_fagsak, fagsak.saksnummer, fagsak.fk_person1_mottaker, fagsak.utbetalingsreferanse
         ,fagsak.vedtaks_tidspunkt
-        ,perioder.pk_pp_perioder, perioder1.dato_fom, perioder1.dato_tom
+        ,perioder.pk_pp_perioder, perioder.dato_fom, perioder.dato_tom
         ,tid.dato
         ,row_number() over (partition by fagsak.fk_person1_mottaker, tid.dato
                             order by fagsak.vedtaks_tidspunkt desc) nr
   from perioder
 
   join tid
-  on tid.dato between perioder.dato_fom and perioder1.dato_tom
+  on tid.dato between perioder.dato_fom and perioder.dato_tom
   and tid.dim_nivaa = 1
   and tid.gyldig_flagg = 1
-  and tid.dato between to_date('01.12.2023','dd.mm.yyyy') and to_date('31.12.2023','dd.mm.yyyy')
+  --and tid.dato between to_date({{ p_in_vedtak_periode_yyyymm }}||'01','yyyymmdd')
+    --                   and last_day(to_date({{ p_in_vedtak_periode_yyyymm }}, 'yyyymm'))
+  and tid.dato between to_date({{var("pp_omp_periode")}}||'01','yyyymmdd')
+                       and last_day(to_date({{var("pp_omp_periode")}}, 'yyyymm'))
 
   join fagsak
   on perioder.fk_pp_fagsak = fagsak.pk_pp_fagsak
   and fagsak.ytelse_type = 'OMP'
   and fagsak.fk_person1_mottaker != -1
-  and fagsak.vedtaks_tidspunkt < sysdate
+  and fagsak.vedtaks_tidspunkt < last_day(to_date({{var("pp_omp_periode")}}, 'yyyymm')) + 1
 
   where perioder.utfall = 'OPPFYLT'
 )
@@ -54,7 +56,7 @@ vedtak1 as
 utbet as
 (
   select *
-  from {{ source('fam_pp', 'fam_pp_periode_utbet_grader') }}
+  from {{ source('fam_pp_omp', 'fam_pp_periode_utbet_grader') }}
 ),
 
 vedtak2 as
@@ -82,3 +84,5 @@ agg as
   from vedtak2
   group by fk_person1_mottaker, dato_fom, dato_tom, saksnummer, utbetalingsreferanse
 )
+select *
+from agg
